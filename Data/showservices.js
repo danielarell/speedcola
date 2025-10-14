45c5487146dd7ccd3128f4da04c8d0a1f16346b1
Juan López <juan@example.com>
2025-10-14 17:10:38 -0600

    showservicesjs commit fixes

--- Data/showservices.js
+++ Data/showservices.js
@@ -16,21 +16,19 @@ document.addEventListener("DOMContentLoaded", async () => {
         col.className = "col-lg-4 col-md-6 align-self-center mb-30 properties-items";
 
         // clase adicional para filtros (str, rac, adv, etc.)
-        if (servicio.categoria.toLowerCase().includes("mechanic")) col.classList.add("str");
-        if (servicio.categoria.toLowerCase().includes("construction")) col.classList.add("rac");
-        if (servicio.categoria.toLowerCase().includes("plumbing")) col.classList.add("adv");
+        // agregar a tabla de servicios imagen
+        //             <a href="property-details.html"><img src="${servicio.imagen}" alt=""></a>
+        // Agregar Rating a servicios
 
         col.innerHTML = `
             <div class="item">
-            <a href="property-details.html"><img src="${servicio.imagen}" alt=""></a>
-            <span class="category">${servicio.categoria}</span>
+            <span class="category">${servicio.idCategoria}</span>
             <h6>$${servicio.precio.toLocaleString()}</h6>
-            <h4><a href="property-details.html">${servicio.ubicacion}</a></h4>
             <ul>
-                <li>Pay method: <span>${servicio.payMethod}</span></li>
-                <li>Provider: <span>${servicio.provider}</span></li>
-                <li>Rating: <span>${servicio.rating}</span></li>
-                <li>Review: <span>${servicio.review}</span></li>
+                <li>nombre del Servicio: <span>${servicio.nombre}</span></li>
+                <li>Descripcion: <span>${servicio.descripcion}</span></li>
+                <li>Provider: <span>${servicio.idUsuario}</span></li>
+                <li>Duracion Estimada: <span>${servicio.duracionEstimada}</span></li>
             </ul>
             <div class="main-button">
                 <a href="property-details.html">Schedule a visit</a>
