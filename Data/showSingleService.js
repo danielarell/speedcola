f4af0817a1d89fb31ba66ce8b37eedd018e25641
Regalado <lregaladofloriano@gmail.com>
2025-10-23 17:37:45 -0600

    chat modal, no css 	modified:   showSingleService.js

--- Data/showSingleService.js
+++ Data/showSingleService.js
@@ -40,7 +40,8 @@ function renderSingleService(servicio) {
 
         <div class="service-actions mt-4">
           <button class="btn btn-chat">
-            <i class="fa fa-comments"></i> Chat
+            <i class="fa fa-comments"></i> 
+            <a href="#" data-bs-toggle="modal" data-bs-target="#chatModal"> Chat </a>
           </button>
           <button class="btn btn-hire">
             <i class="fa fa-briefcase"></i> Contratar servicio
@@ -48,6 +49,86 @@ function renderSingleService(servicio) {
         </div>
       </div>
     </div>
+
+
+<div class="modal fade " id="collapseOne">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+          <ul class="chat">
+              <li class="left clearfix"><span class="chat-img pull-left">
+                  <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+              </span>
+                  <div class="chat-body clearfix">
+                      <div class="header">
+                          <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                              <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                      </div>
+                      <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                          dolor, quis ullamcorper ligula sodales.
+                      </p>
+                  </div>
+              </li>
+              <li class="right clearfix"><span class="chat-img pull-right">
+                  <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
+              </span>
+                  <div class="chat-body clearfix">
+                      <div class="header">
+                          <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                          <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                      </div>
+                      <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                          dolor, quis ullamcorper ligula sodales.
+                      </p>
+                  </div>
+              </li>
+              <li class="left clearfix"><span class="chat-img pull-left">
+                  <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+              </span>
+                  <div class="chat-body clearfix">
+                      <div class="header">
+                          <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                              <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+                      </div>
+                      <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                          dolor, quis ullamcorper ligula sodales.
+                      </p>
+                  </div>
+              </li>
+              <li class="right clearfix"><span class="chat-img pull-right">
+                  <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
+              </span>
+                  <div class="chat-body clearfix">
+                      <div class="header">
+                          <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>15 mins ago</small>
+                          <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                      </div>
+                      <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                          dolor, quis ullamcorper ligula sodales.
+                      </p>
+                  </div>
+              </li>
+          </ul>
+      </div>
+      <div class="modal-footer">
+        <div class="input-group">
+            <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+            <span class="input-group-btn">
+                <button class="btn btn-warning btn-sm" id="btn-chat">
+                    Send</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </span>
+        </div>
+      </div>
+    </div>
+</div>
+  </div>
+</div>
+
   `;
 }
 
